db322ad561d84f0ecc3cb241c588c31146938025
junhyeong.jeon <junhyeong.jeon@polycube.co.kr>
2018-07-16 14:50:51 +0900

    fix

--- android/src/main/java/com/cunyutech/hollyliu/reactnative/wallpaper/WallPaperManager.java
+++ android/src/main/java/com/cunyutech/hollyliu/reactnative/wallpaper/WallPaperManager.java
@@ -78,7 +78,7 @@ public class WallPaperManager extends ReactContextBaseJavaModule {
         int whichScreen = WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM;
         if (params.hasKey("screen")) {
           whichScreen = params.getString("screen") == "lock" ? WallpaperManager.FLAG_LOCK : WallpaperManager.FLAG_SYSTEM;
-          sendMessage(params.getString("screen") + params.getString("screen") == "lock" ? " true" : " false");
+          sendMessage("log", params.getString("screen") + params.getString("screen") == "lock" ? " true" : " false", source);
         }
 
         if(rctCallback!=null){
